1455dc331a6838cce24ff371c8468ab63c577eff
Fearhad <allan.hartigan@gmail.com>
2018-08-04 02:53:49 -0400

    added all the questions as objects inside an array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,185 @@
+var questionList = [{
+        question: "What are Durin's Folk more commonly known as?",
+        answer1: "Hobbits",
+        answer2: "Dwarves",
+        answer3: "Forest Elves",
+        answer4: "Great Eagles",
+        correct: "Dwarves",
+        image: "https://i.pinimg.com/originals/eb/d8/2e/ebd82e04346f4bc192dd399a8000b516.jpg",
+        flavor: "Unlike Elves and Men, the Dwarves are not Children of Ilúvatar; they were created by Aulë the Smith, though Ilúvatar granted them life. Aulë made seven Fathers of the Dwarves, and these slept through many ages until after the Awakening of the Elves. Almost all the Dwarves that appear in Tolkien's works were descended from the eldest of the Seven Fathers, Durin the Deathless."
+    },
+    {
+        question: "Where was the Bridge of Khazad-Dum?",
+        answer1: "Brandywine River",
+        answer2: "Valinor",
+        answer3: "Mordor",
+        answer4: "Moria",
+        correct: "Moria",
+        image: "https://4.bp.blogspot.com/-b5RvkUUqWps/W2Ri0YrNhNI/AAAAAAAAaBo/k4y7rB53OlUlB38I5aiGV9Rw3ntn7UPSQCLcBGAs/s320/124097.jpg",
+        flavor: "Most famously, the Bridge was the site of Gandalf's encounter with the Balrog of Moria on 15 January III 3019. In the battle that followed, the Bridge was broken, and the First Hall was left isolated from the rest of Khazad-dûm."
+    },
+    {
+        question: "What kind of creatures are the spawn of Ungoliant?",
+        answer1: "Hill Giants",
+        answer2: "Uruk-hai",
+        answer3: "Giant Spiders",
+        answer4: "Flying beasts",
+        correct: "Giant Spiders",
+        image: "https://i2.wp.com/horrorpedia.com/wp-content/uploads/2015/07/lavalantula-2015-lava-breathing-tarantula-giant-spider.jpg?fit=1200%2C677&ssl=1&resize=350%2C200",
+        flavor: "The evil creature of spider-form who was Melkor's accomplice in the Darkening of Valinor, and who fled with him to Middle-earth. The creatures that infested the Ered Gorgoroth were her offspring and descendants, as was Shelob who dwelt on the borders of Mordor."
+    },
+    {
+        question: "Which of these is not an inn in Middle-Earth?",
+        answer1: "Bridge Inn",
+        answer2: "The Green Dragon",
+        answer3: "The Prancing Pony",
+        answer4: "The Southern Star",
+        correct: "The Southern Star",
+        image: "http://4.bp.blogspot.com/-gFg0mm79USQ/VfjGREV9erI/AAAAAAAAChQ/qDDwN6EAJig/s320/1.4%2BPrancing%2BPony.jpg",
+        flavor: "The Prancing Pony was the Inn of Bree, whose landlord at the time of the War of the Ring was Barliman Butterbur.One of the Shire's many inns, the Green Dragon stood on the Bywater Road, the last building passed by travellers from Bywater on their way towards Hobbiton. The Bridge Inn was an inn built on the western banks of the River Brandywine. Southern Star is Pipeweed"
+    },
+    {
+        question: "Who attacked Isengard in the Battle of Isengard?",
+        answer1: "Rohirrim",
+        answer2: "Ents",
+        answer3: "Galadhrim",
+        answer4: "Gondor",
+        correct: "Ents",
+        image: "https://www.artleo.com/mini/201111/14672.jpg",
+        flavor: "A race of giant, tree-like people whose purpose was to protect the forests of Middle-earth. By the end of the Third Age, the great forests had dwindled, and so had the Ents, but they were still to be found in Fangorn Forest."
+    },
+    {
+        question: "Which of these weapons was not found in the troll's cave?",
+        answer1: "Orcrist",
+        answer2: "Glamdring",
+        answer3: "Sting",
+        answer4: "Angrist",
+        correct: "Angrist",
+        image: "https://i.pinimg.com/originals/c9/b8/97/c9b897501a7bc33288129521068efa8d.gif",
+        flavor: "Angrist was an ancient sword forged by Telchar of Nogrod. It was used to pry one of the Silmaril jewels out of Melkor's Iron Crown."
+    },
+    {
+        question: "Who was Elanor Gardner?",
+        answer1: "Daughter of Samwise",
+        answer2: "Queen of Mirkwood",
+        answer3: "One of the Nine",
+        answer4: "Wife of Bard",
+        correct: "Daughter of Samwise",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBj9UbRTEvhHCPqCB1i_jJiMzHMxQ9eTLncSpFF50lu_0Kc53wHw",
+        flavor: "Elanor was one of twelve children that Samwise Gamgee and Rose Cotton had together."
+    },
+    {
+        question: "What did Aragorn, Imrahil, Gandalf, Eomer, Elladan, and Elrohir decide during the Last Debate?",
+        answer1: "The Ring-Bearer",
+        answer2: "White Council Leader",
+        answer3: "How many soldiers to send to the Morannon",
+        answer4: "Punish Uruk-hai",
+        correct: "How many soldiers to send to the Morannon",
+        image: "http://1.bp.blogspot.com/-u1mnQudb3ec/TlkvZJlpcMI/AAAAAAAABmM/k9fXQWXRkeE/s320/rivendell2.jpg",
+        flavor: "After the Last Debate, Aragorn led a force of 6,000 soldiers in order to distract Sauron's forces so that Frodo could slip in and destroy the One Ring."
+    },
+    {
+        question: "What Sindarin word was Aragorn known by when he was a child?",
+        answer1: "Estel",
+        answer2: "Amdir",
+        answer3: "Aeluin",
+        answer4: "Miriel",
+        correct: "Estel",
+        image: "http://i9.lisimg.com/14915279/320full.jpg",
+        flavor: "Aragorn lived in Rivendell as a child. He was called Estel in order to conceal his identity from Sauron's forces."
+    },
+    {
+        question: "What happened to Lobelia Sackville-Baggins after the War of the Ring?",
+        answer1: "Imprisoned",
+        answer2: "She died",
+        answer3: "Became Ringwraith",
+        answer4: "Went to Grey Havens",
+        correct: "She died",
+        image: "http://4.bp.blogspot.com/-FzI5w9UvNP4/U2-uamoiEtI/AAAAAAAAXnk/qvyrhAuU9-0/s1600/lord_017.png",
+        flavor: "Lobelia Sackville-Baggins makes peace with the Baggins family, and upon her death, donates her wealth in order to find homes for all the hobbits who were displaced by her son, Lotho."
+    },
+    {
+        question: "What do the Elves call the Hobbits?",
+        answer1: "The Melion",
+        answer2: "The Periannath",
+        answer3: "The Onodrim",
+        answer4: "The Uruloki",
+        correct: "The Periannath",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR73IdtbmI8LR_hjsWZ7eLlo51ca-Ukpuw3kD7jTljVECNahX5P8w",
+        flavor: "The Sindarin word perian literally translates to halfling. Periannath is the collective plural."
+    },
+    {
+        question: "What marked the start of the Third Age?",
+        answer1: "Defeat of Sauron",
+        answer2: "Destruction of Beleriand",
+        answer3: "Death of Sauron",
+        answer4: "Creation of The Two Trees",
+        correct: "Defeat of Sauron",
+        image: "https://cdn.thinglink.me/api/image/7DceLftyMsyaisbyRMiwkhVXtt6EQsYXUzjR46vej5iTqJR5KZZr5mijaY6kitoc7LwYHJPdhUvuaQuxow5Ci/320/320/scaledown",
+        flavor: "The Third Age began with the Last Alliance of Elves and Men defeating Sauron and ended with the destruction of the One Ring and Sauron's death."
+    },
+    {
+        question: "Where is Wellinghall?",
+        answer1: "Inside an Entish Castle",
+        answer2: "Shelob's Lair",
+        answer3: "Isengard",
+        answer4: "Where Merry and Pippen drank Ent-Draughts",
+        correct: "Where Merry and Pippen drank Ent-Draughts",
+        image: "http://csdb.dk/gfx/releases/46000/46425.gif",
+        flavor: "Wellinghall is where Merry and Pippin drank the Ent-draughts that made them grow to be the two tallest hobbits in all of the Shire."
+    },
+    {
+        question: "Who was Snaga?",
+        answer1: "Great Goblin",
+        answer2: "Fire Drake",
+        answer3: "Orc-Chieftan of Moria",
+        answer4: "Orc that Sam Killed in Mordor",
+        correct: "Orc that Sam Killed in Mordor",
+        image: "http://1.bp.blogspot.com/-tDAobL_QsHQ/TmqGdDHmpyI/AAAAAAAABlE/xwWN-fEmCDE/s1600/goblin.gif",
+        flavor: "Snaga may actually not be a specific name, but a mocking term used by Uruk-hai and Orc chieftains meant to belittle lesser Orcs."
+    },
+    {
+        question: "Which of these is not an Elvish language?",
+        answer1: "Quenya",
+        answer2: "Taliska",
+        answer3: "Telerin",
+        answer4: "Sindarin",
+        correct: "Taliska",
+        image: "http://3.bp.blogspot.com/_pSH4gVYXKIs/SfUrlQdiEUI/AAAAAAAAACE/fwxxijh6MJo/s320/ganda.JPG",
+        flavor: "Taliska is a language of Man. It was spoken by the Edain of the House of Bëor and House of Hador."
+    },
+    {
+        question: "How does Farmer Maggot keep people off his farm, Bamfurlong?",
+        answer1: "Trained Dogs",
+        answer2: "Trained Wolves",
+        answer3: "A Beorning",
+        answer4: "Bee Swarms",
+        correct: "Trained Dogs",
+        image: "https://africageographic.com/wp-content/uploads/2015/05/wild-dog-pack-serengeti-painted-wolves-project-320x200.jpg",
+        flavor: "Farmer Maggot's three dogs were called Grip, Fang, and Wolf."
+    },
+    {
+        question: "What is the Front Porch?",
+        answer1: "Tavern in Bree",
+        answer2: "Plains east of Mordor",
+        answer3: "Hidden entrance to Goblin-Town",
+        answer4: "Bilbo's house",
+        correcT: "Hidden entrance to Goblin-Town",
+        image: "https://cdn.4archive.org/img/f74bJALm.jpg",
+        flavor: "The Front Porch is where Bilbo Baggins and the dwarves take shelter from the thunder-battle. While they slept, goblins abducted the party using hidden entrances into the cave."
+    },
+    {
+        question: "What are the Palantiri and how many are there?",
+        answer1: "Three Elven Rings",
+        answer2: "Seven Seeing Stones",
+        answer3: "Three Jewels of Feanor",
+        answer4: "Three strands of Galadriel's hair",
+        correct: "Seven Seeing Stones",
+        image: "https://i.redditmedia.com/3QweNkwRzZanJNdm5Xuu3Bj1o5pRTYsvR8mNwm5Lyg0.jpg?w=320&s=0d83aae892b831ab104921a045ecb63a",
+        flavor: "The Palantíri were magical objects that could be used to see and communicate over great distances."
+    }
+];
+
 
 var menuSong = $("#menuMusic");
 var gameSong = $("#gameMusic");
@@ -12,22 +194,25 @@ var game = {
         displayScreens.forEach(function (currentDisplay) {
             console.log(currentDisplay);
             console.log(activeDisplay);
-          if (currentDisplay == activeDisplay[0]) {
-            $(currentDisplay).css("display", "block");
-          } else {
-            $(currentDisplay).css("display", "none");
-          }
+            if (currentDisplay == activeDisplay[0]) {
+                $(currentDisplay).css("display", "block");
+            } else {
+                $(currentDisplay).css("display", "none");
+            }
         })
-      },
+    },
+    initGame: function() {
+    
+    }
 
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     function mainMenu() {
         game.showScreen(gameMenu);
         menuSong[0].play();
-      }
-      mainMenu();
+    }
+    mainMenu();
 });
 
 document.querySelectorAll('#play')[0].addEventListener('click', function () {
@@ -35,11 +220,11 @@ document.querySelectorAll('#play')[0].addEventListener('click', function () {
     // game.gameUI.initGame();
     menuSong[0].pause();
     gameSong[0].play();
-  });
+});
 
-  document.querySelectorAll('#credits')[0].addEventListener('click', function () {
+document.querySelectorAll('#credits')[0].addEventListener('click', function () {
     menuSong[0].pause();
     game.showScreen(gameCredits);
     creditsSong[0].play();
-  
-  });
+
+});
