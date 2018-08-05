5b0949597f056025b6f32418c2911f29f80645df
Fearhad <allan.hartigan@gmail.com>
2018-08-05 12:08:26 -0400

    finished game logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -196,7 +196,7 @@ var displayScreens = [menuScreen, gameScreen, creditsScreen, OverScreen];
 var highScore = 0;
 var currentQuestion;
 var timeLeft;
-var questionsLeft = 10;
+var questionsLeft = 1;
 var score = 0;
 var correctAnswers = 0;
 var wrongAnswers = 0;
